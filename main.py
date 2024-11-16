a8f1b9cc7a0b6c806b91d1f93567bf62b6336225
guilherme.barroso <guilherme.barroso@luizalabs.com>
2024-11-16 16:22:49 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI, HTTPException
+import os
+from dotenv import load_dotenv
+from datetime import datetime
+
+load_dotenv()
+app = FastAPI()
+
+
+
+@app.get("/")
+async def hello_world():
+    return {"message": "Hello world!"}
+
+
